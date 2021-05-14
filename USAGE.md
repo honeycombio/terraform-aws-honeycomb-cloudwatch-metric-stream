5b2b0a30cbf94975d0f04b11df210e8e95bc48a8
Ian Smith <iansmith@honeycomb.io>
2021-05-13 20:16:42 -0700

    Remove terraform-docs workflow for now and just do it manually.

--- USAGE.md
+++ USAGE.md
@@ -3,6 +3,7 @@
 
 | Name | Version |
 |------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13 |
 | <a name="requirement_aws"></a> [aws](#requirement\_aws) | > 0.0.0 |
 
 ## Providers
--- docs.sh
+++ docs.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 # This script generates the docs that .github/workflows/terraform-docs.yml
-# would, but cannot push due to repo ACLs. So instead that workflow just checks
-# that USAGE.md is what it expects, and fails the build if it is not.
+# would, but cannot push due to repo ACLs.
 
 if ! (command -v terraform-docs >/dev/null); then
   echo "You need terraform-docs:"

c3780590e0e14477b333a48b4c8e8849930b5224
Ian Smith <iansmith@honeycomb.io>
2021-06-08 11:30:27 -0700

    No need to prefix our sources with app.terraform.io once the module is in the public registry (#17)

--- examples/with-include-filters.tf
+++ examples/with-include-filters.tf
@@ -1,5 +1,5 @@
 module "cloudwatch_metric_stream_with_includes" {
-  source = "app.terraform.io/honeycomb/honeycomb-cloudwatch-metric-stream/aws"
+  source = "honeycomb/honeycomb-cloudwatch-metric-stream/aws"
 
   name                   = "cms_with_includes"
   honeycomb_dataset_name = "cloudwatch-with-includes"

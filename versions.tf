4d4eb7ebd1b3427aa680461e583e67814f830ddb
Jason Harley <jason@honeycomb.io>
2022-09-09 14:55:00 -0400

    Update AWS provider constrains to take AWS v4 Bucket Refactor into account
    Prior to v4.9.0 of the AWS provider the S3 bucket resource had a handful of breaking changes.
    This change will allow compatible v3 and v4 versions of the AWS provider to use this module safely.

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  # TODO: not sure we need >= 0.13! But it's what we've tested against.
   required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.42.0"
+      version = ">= 3.42.0, >= 4.9.0"
     }
   }
 }

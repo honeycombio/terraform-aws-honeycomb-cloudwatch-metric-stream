9bf2df692e8511439a02542403cb86c265ebe3da
Jason Harley <jason@honeycomb.io>
2022-09-12 11:17:15 -0400

    Properly exclude AWS provider versions 4.0->4.8 (#23)

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.42.0, >= 4.9.0"
+      version = ">= 3.42.0, != 4.0.0, != 4.1.0, != 4.2.0, != 4.3.0, != 4.4.0, != 4.5.0, != 4.6.0, != 4.7.0, != 4.8.0"
     }
   }
 }
